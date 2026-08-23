0891ab699a52a0f16d50696c89c0f5f07236dbd4
Lukasz <setlaklukasz@gmail.com>
2026-08-23 13:30:06 +0200

    Realign App.tsx with component convention

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from 'react-router/dom';
 
 import { router } from './routes';
 
-export function App() {
+const App = () => {
   return <RouterProvider router={router} />;
-}
+};
+
+export default App;
