52e2b5bc970998f322619dffdd3c2a76aeddd37a
Lukasz <setlaklukasz@gmail.com>
2026-08-25 14:08:31 +0200

    Update contest routes

--- apps/api/src/modules/contests/routes.ts
+++ apps/api/src/modules/contests/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { submissionsRoutes } from '../submissions/routes.js';
 import { getOne, list, verifyAccess } from './controller.js';
 
 export const contestsRoutes = Router();
@@ -6,3 +7,4 @@ export const contestsRoutes = Router();
 contestsRoutes.get('/', list);
 contestsRoutes.get('/:slug', getOne);
 contestsRoutes.post('/:slug/access', verifyAccess);
+contestsRoutes.use('/:slug/submissions', submissionsRoutes);
