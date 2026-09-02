ae61f7d71919f3c6f985ab9e0a89c10551d130ba
Lukasz <setlaklukasz@gmail.com>
2026-09-02 13:56:41 +0200

    Increase file size limi

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,6 +1,6 @@
 import { NICKNAME_POOL } from './nickname-pool.js';
 
-export const MAX_ARTWORK_FILE_SIZE_BYTES = 15 * 1024 * 1024;
+export const MAX_ARTWORK_FILE_SIZE_BYTES = 25 * 1024 * 1024;
 export const MIN_ARTWORK_DIMENSION_PX = 800;
 
 export const ALLOWED_ARTWORK_MIME_TYPES = ['image/jpeg', 'image/png'] as const;
