3ca405781d13541a168f877fa44addcaf7976da8
Lukasz <setlaklukasz@gmail.com>
2026-09-07 18:00:02 +0200

    Add i18next configuration and localization support for English and Polish

---
+++ apps/web/src/i18n/config.ts
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+import en from './locales/en/translation.json';
+import pl from './locales/pl/translation.json';
+
+void i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      pl: { translation: pl },
+    },
+    fallbackLng: 'pl',
+    supportedLngs: ['pl', 'en'],
+    nonExplicitSupportedLngs: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      lookupLocalStorage: 'foka-lang',
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18next;
